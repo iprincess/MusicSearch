49039c37068b38f99c14013c214b587ef75c88cc
Melissa <lanemelissa_99@yahoo.com>
2020-07-03 17:43:35 -0700

    Update webpack.prod.js
    Added desktop config to webpack.prod

--- MusicSearchApp/ClientApp/webpack.prod.js
+++ MusicSearchApp/ClientApp/webpack.prod.js
@@ -1,64 +1,76 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = [
   merge(common.configs[0], {
     mode: "production",
-    devtool: 'source-map',
+    devtool: "source-map",
     optimization: {
       minimize: true,
-      minimizer: [
-        new TerserPlugin(), 
-        new OptimizeCSSAssetsPlugin({})
-      ],
+      minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
     },
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, common.cssLoader, common.postCssLoader],
+          use: [
+            MiniCssExtractPlugin.loader,
+            common.cssLoader,
+            common.postCssLoader,
+          ],
         },
         {
           test: /\.s[ac]ss$/i,
-          use: [MiniCssExtractPlugin.loader, common.cssLoader, common.postCssLoader, common.sassLoader],
+          use: [
+            MiniCssExtractPlugin.loader,
+            common.cssLoader,
+            common.postCssLoader,
+            common.sassLoader,
+          ],
         },
       ],
     },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: '[name].[hash].css',
+        filename: "[name].[hash].css",
       }),
     ],
   }),
-  // merge(common.configs[1], {
-  //   mode: "production",
-  //   devtool: 'source-map',
-  //   optimization: {
-  //     minimize: true,
-  //     minimizer: [
-  //       new TerserPlugin(), 
-  //       new OptimizeCSSAssetsPlugin({})
-  //     ],
-  //   },
-  //   module: {
-  //     rules: [
-  //       {
-  //         test: /\.css$/i,
-  //         use: [MiniCssExtractPlugin.loader, common.cssLoader, common.postCssLoader],
-  //       },
-  //       {
-  //         test: /\.s[ac]ss$/i,
-  //         use: [MiniCssExtractPlugin.loader, common.cssLoader, common.postCssLoader, common.sassLoader],
-  //       },
-  //     ],
-  //   },
-  //   plugins: [
-  //     new MiniCssExtractPlugin({
-  //       filename: '[name]-desktop.[hash].css',
-  //     }),
-  //   ],
-  // }),
+  merge(common.configs[1], {
+    mode: "production",
+    devtool: "source-map",
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            common.cssLoader,
+            common.postCssLoader,
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            common.cssLoader,
+            common.postCssLoader,
+            common.sassLoader,
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name]-desktop.[hash].css",
+      }),
+    ],
+  }),
 ];
